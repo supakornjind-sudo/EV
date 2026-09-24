88adbb410908018bb02f359f47f4bfa6fea94104
supakornjind-sudo <supakorn.jind@gmail.com>
2026-09-24 09:40:15 +0700

    Update API_URL in config.js to new endpoint

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * หลัง Deploy Apps Script แล้ว ให้วาง URL ที่ได้มาแทนค่าด้านล่างนี้
  */
 const CONFIG = {
-  API_URL: 'วาง_URL_APPS_SCRIPT_ของเว็บนี้_ที่นี่',
+  API_URL: 'https://script.google.com/macros/s/AKfycbybDQ9WpEMHOGXsuFma2q8JQxcmXVLfxlP8WNJu1ZYcfhgSeegq5p3d9ipoHQLgmHl6/exec',
   APP_TITLE: 'ปฏิทินงานประจำเดือน',
 };
