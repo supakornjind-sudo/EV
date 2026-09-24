3b8786fae16f25292829fb39c6aaa5b6953116f2
supakornjind-sudo <supakorn.jind@gmail.com>
2026-09-24 09:57:34 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,6 +3,6 @@
  * หลัง Deploy Apps Script แล้ว ให้วาง URL ที่ได้มาแทนค่าด้านล่างนี้
  */
 const CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbybDQ9WpEMHOGXsuFma2q8JQxcmXVLfxlP8WNJu1ZYcfhgSeegq5p3d9ipoHQLgmHl6/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwhjtv-XYbWlaYggrBF9Cr_UTPRpTgsE497piCHnEy2cH4JvoUkW3MrPKIvVOl0Apa_/exec',
   APP_TITLE: 'ปฏิทินงานประจำเดือน',
 };
